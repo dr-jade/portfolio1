48e474d978262047e6266c6154363395a98a4891
dr-jade <dr.jadelee@gmail.com>
2022-12-12 02:24:55 -0800

    mobile version done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// sidebar
+//sidebar
 $("nav").attr("height", screen.height);
 $("nav").addClass("close");
 if($("body").innerWidth() < 400) $("nav").addClass("narrow");
@@ -8,7 +8,6 @@ $("#handle").on("click", function(){
         $("nav").toggleClass("narrow");
     }
 })
-
 // gallery
 const jsonLoad = async() => {
     const loadData = new Promise((res)=>{
@@ -33,35 +32,48 @@ const jsonLoad = async() => {
         (err)=>{console.log(err)}
     )
 }
-$.when(jsonLoad()).then(function setSlide(){
-    let slides = $(".gal").children("ul").children("li");
-    let wrapWidth = $(".works").width();
-    $(slides).css("width", wrapWidth);
-    let slCount = slides.length;
-    let slWidth =  slides.width();
-    let slHeight = slides.height();
-    let sliderWidth = slCount * slWidth;
-    $(".gal").css({"max-width": slWidth, "height": slHeight});
-    $(".gal ul").css({"width": sliderWidth, "margin-left": -slWidth});
-    $(".gal ul li:last-child").prependTo($(".gal ul"));
-    $(".gal ul li:last-child").appendTo($(".gal ul"));
-    $(".prev").on("click", function(){
-        $(".gal ul").stop().animate({
-            left: + slWidth
-        }, 700, function(){
-            $(".gal ul li:last-child").prependTo($(".gal ul"));
-            $(".gal ul").css("left", "");
+$.when(jsonLoad()).then(function(){
+    window.setTimeout(function setSlide(){
+        let slides = $(".gal ul").children("li");
+        let wrapWidth = $(".gal").width();
+        $(slides).css("width", wrapWidth);
+        let slCount = slides.length;
+        let slWidth =  slides.width();
+        let slHeight = slides.height();
+        let sliderWidth = slCount * slWidth;
+        $(".gal").css({"min-width": wrapWidth, "max-width": slWidth, "height": slHeight});
+        $(".gal ul").css({"width": sliderWidth, "margin-left": -slWidth});
+        $(".gal ul li:last-child").prependTo($(".gal ul"));
+        $(".gal ul li:last-child").appendTo($(".gal ul"));
+        $(".prev").on("click", function(){
+            $(".gal ul").stop().animate({
+                left: + slWidth
+            }, 700, function(){
+                $(".gal ul li:last-child").prependTo($(".gal ul"));
+                $(".gal ul").css("left", "");
+            })
         })
-    })
-    $(".next").on("click", function(){
-        $(".gal ul").stop().animate({
-            left: - slWidth
-        }, 700, function(){
-            $(".gal ul li:first-child").appendTo($(".gal ul"));
-            $(".gal ul").css("left", "");
+        $(".next").on("click", function(){
+            $(".gal ul").stop().animate({
+                left: - slWidth
+            }, 700, function(){
+                $(".gal ul li:first-child").appendTo($(".gal ul"));
+                $(".gal ul").css("left", "");
+            })
         })
-    })
-    $(window).resize(function(){
-        setSlide();
-    })
+        // class observer
+        let nav = $("nav")[0];
+        const options = {
+            attributes: true
+        }
+        function callBack(muteList, observer){
+            muteList.forEach(function(mutes){
+                if(mutes.type === "attributes" && mutes.attributeName === "class"){
+                    setSlide();
+                }
+            })
+        }
+        const observer = new MutationObserver(callBack);
+        observer.observe(nav, options);
+    }, 500)
 })
