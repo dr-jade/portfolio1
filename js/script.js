2b91cd08e7f6661864397540b48d71582f1ec885
dr-jade <dr.jadelee@gmail.com>
2022-12-11 22:28:06 -0800

    gallery done

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ $("#handle").on("click", function(){
 })
 
 // gallery
-let gal = $("article")[6];
 const jsonLoad = async() => {
     const loadData = new Promise((res)=>{
         let xmlReq = new XMLHttpRequest();
@@ -21,15 +20,48 @@ const jsonLoad = async() => {
         xmlReq.send();
     }).then(
         (data)=>{
+            // Add images
             let links = JSON.parse(data);
+            let ul = $("<ul></ul>");
             $.each(links, function(idx, val){
-                let img = $("<img>");
-                img.attr("src", val.src);
-                img.attr("alt", "pic"+idx);
-                $(gal).append(img);
+                let li = $("<li></li>");
+                li.css("background-image", "url("+val.src+")");
+                $(ul).append(li);
+                $(".gal").append(ul);
             })
         },
         (err)=>{console.log(err)}
     )
 }
-jsonLoad();
+$.when(jsonLoad()).then(function setSlide(){
+    let slides = $(".gal").children("ul").children("li");
+    let wrapWidth = $(".works").width();
+    $(slides).css("width", wrapWidth);
+    let slCount = slides.length;
+    let slWidth =  slides.width();
+    let slHeight = slides.height();
+    let sliderWidth = slCount * slWidth;
+    $(".gal").css({"max-width": slWidth, "height": slHeight});
+    $(".gal ul").css({"width": sliderWidth, "margin-left": -slWidth});
+    $(".gal ul li:last-child").prependTo($(".gal ul"));
+    $(".gal ul li:last-child").appendTo($(".gal ul"));
+    $(".prev").on("click", function(){
+        $(".gal ul").stop().animate({
+            left: + slWidth
+        }, 700, function(){
+            $(".gal ul li:last-child").prependTo($(".gal ul"));
+            $(".gal ul").css("left", "");
+        })
+    })
+    $(".next").on("click", function(){
+        $(".gal ul").stop().animate({
+            left: - slWidth
+        }, 700, function(){
+            $(".gal ul li:first-child").appendTo($(".gal ul"));
+            $(".gal ul").css("left", "");
+        })
+    })
+    $(window).resize(function(){
+        setSlide();
+    })
+})
